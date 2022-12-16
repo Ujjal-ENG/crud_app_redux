4b60bbe1dbe47f3808f04e01035aa73038ee78d7
user <ujjalroy7862@gmail.com>
2022-12-16 20:57:24 +0600

    link the navbar

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddBook from "../features/books/AddBook";
+import BooksView from "../features/books/BooksView";
 import Navbar from "../layouts/Navbar";
 import Error from "../pages/Error";
 import Home from "../pages/Home";
@@ -10,6 +12,8 @@ const Index = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/show-books" element={<BooksView />} />
+        <Route path="/add-book" element={<AddBook />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
