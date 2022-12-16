b2c9a4e556b9ae60c8f214715dfa30f8ca485382
user <ujjalroy7862@gmail.com>
2022-12-16 23:42:30 +0600

    done this BookView Part

--- src/features/books/BooksView.jsx
+++ src/features/books/BooksView.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const BooksView = () => {
-  return (
-    <div>BooksView</div>
-  )
-}
+  const books = useSelector((state) => state.booksReducer.books);
 
-export default BooksView
+  return (
+    <div>
+      <h2>List Of Books</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books &&
+            books.map((book) => {
+              const { id, title, author } = book;
+              return (
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td>{title} </td>
+                  <td>{author}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BooksView;
